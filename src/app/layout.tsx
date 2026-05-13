e9c9e3bc91de72e863ea97beb8b4b1ab19f8fb9e
vcezar-dev <vcezaroliveiraa@gmail.com>
2026-05-12 21:52:41 -0300

    chore: reorganize project structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
-import './globals.css';
+import '@/styles/globals.css';
 import { Geist } from "next/font/google";
 import { cn } from "@/lib/utils";
 
 const geist = Geist({subsets:['latin'],variable:'--font-sans'});
-
 
 export default function RootLayout({
     children,
